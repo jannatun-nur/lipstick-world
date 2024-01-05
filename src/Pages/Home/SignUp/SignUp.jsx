9a249e01541d693e6394314b65bc522a82745714
jannatun-nur <jannatunur03@gmail.com>
2024-01-05 15:52:51 +0600

    signup 100%

--- src/Pages/Home/SignUp/SignUp.jsx
+++ src/Pages/Home/SignUp/SignUp.jsx
@@ -2,6 +2,7 @@ import { useContext, useState } from "react";
 import { AuthContext } from '../../../Provider/AuthProvider';
 import Swal from 'sweetalert2'
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import image from '../../../../public/assets/image/signup.jpg'
 // import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const SignUp = () => {
@@ -61,17 +62,17 @@ createUserWithEmailAndPassword(email , password)
 
     return (
         <div>
-            <div className="">
-  <div className="e">
-    {/* <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-    </div> */}
-    <div className="w-full  bg-white">
-    <div className="flex flex-col max-auto p-6 rounded-md sm:p-10  w-1/2">
+            <div className="bg-[#f4f4f4]">
+  <div className=" flex justify-around">
+    <div className=" mt-10">
+        <img src={image} alt="image" />
+    </div>
+   
+    <div className="w-1/2  text-gray-800">
+    <div className="flex flex-col max-auto p-6 rounded-md sm:p-10 w-full">
 	<div className="mb-8 text-center">
 		
-		<p className="text-sm lg:text-2xl text-cyan-700 font-serif">Sign Up to access your account</p>
+		<p className="text-sm lg:text-2xl text-pink-700 border border-x-pink-600 font-serif">Sign Up to access your account</p>
 	</div>
 	<form  onSubmit={handleSignup} className="space-y-12">
 		<div className="space-y-4">
@@ -80,34 +81,34 @@ createUserWithEmailAndPassword(email , password)
 					<label  className="text-sm text-white font-semibold">Name</label>
 					
 				</div>
-				<input type="Username" name="name" id="Username" placeholder="Username" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none" />
+				<input type="Username" name="name" id="Username" placeholder="Username" className="w-full px-3 py-2 border rounded-md bg-white  border-pink-600 hover:boder-pink-300" />
 			</div>
 			<div>
 				<div className="flex justify-between mb-2 text-white font-semibold">
 					<label  className="text-sm">Address</label>
 					
 				</div>
-				<input type="address" name="address" id="address" placeholder="address" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none" />
+				<input type="address" name="address" id="address" placeholder="address" className="w-full px-3 py-2 bg-white rounded-md border border-pink-600 hover:boder-pink-300" />
 			</div>
 			<div>
 				<label  className="block mb-2 text-sm text-white font-semibold">Email address</label>
-				<input type="email" name="email" id="email" placeholder="@gmail.com" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none" />
+				<input type="email" name="email" id="email" placeholder="@gmail.com" className="w-full px-3 py-2 bg-white rounded-md border border-pink-600 hover:boder-pink-300" />
 			</div>
 			<div>
 				<div className="flex justify-between mb-2 text-white font-semibold">
 					<label  className="text-sm">Confirm Password</label>
 					
 				</div>
-				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none " />
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 bg-white rounded-md border border-pink-600 hover:boder-pink-300 " />
 			</div>
 			
 		</div>
 		<div className="space-y-2">
 			<div>
-				<button type="submit" className="w-full px-8 py-3 font-semibold rounded-md bg-cyan-700 text-white font-serif">Sign Up</button>
+				<button type="submit" className="px-8 py-3 font-semibold rounded-md bg-pink-700 text-white font-serif">Sign Up</button>
                
             </div>
-			<p className="px-6 text-sm text-center text-cyan-700 font-serif">Already have accout?
+			<p className="px-6 text-sm text-center text-pink-700 font-serif">Already have accout?
 				{/* <Link to="/login">Login Here</Link> */}
 			</p>
 		</div>
