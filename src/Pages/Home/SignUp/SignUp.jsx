2b7ed26bbb269eef6f4a5086289a5d90bd5cf1cd
jannatun-nur <jannatunur03@gmail.com>
2024-01-05 09:08:45 +0600

    signup 30%

--- src/Pages/Home/SignUp/SignUp.jsx
+++ src/Pages/Home/SignUp/SignUp.jsx
@@ -3,7 +3,68 @@
 const SignUp = () => {
     return (
         <div>
-            
+            <div className="">
+  <div className="e">
+    {/* <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Login now!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+    </div> */}
+    <div className="w-full  bg-white">
+    <div className="flex flex-col max-auto p-6 rounded-md sm:p-10  w-1/2">
+	<div className="mb-8 text-center">
+		
+		<p className="text-sm lg:text-2xl text-cyan-700 font-serif">Sign Up to access your account</p>
+	</div>
+	<form  className="space-y-12">
+		<div className="space-y-4">
+        <div>
+				<div className="flex justify-between mb-2 text-white font-semibold">
+					<label  className="text-sm text-white font-semibold">Name</label>
+					
+				</div>
+				<input type="Username" name="Username" id="Username" placeholder="Username" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none" />
+			</div>
+			<div>
+				<div className="flex justify-between mb-2 text-white font-semibold">
+					<label  className="text-sm">Address</label>
+					
+				</div>
+				<input type="address" name="address" id="address" placeholder="address" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none" />
+			</div>
+			<div>
+				<label  className="block mb-2 text-sm text-white font-semibold">Email address</label>
+				<input type="email" name="email" id="email" placeholder="@gmail.com" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none" />
+			</div>
+			<div>
+				<div className="flex justify-between mb-2 text-white font-semibold">
+					<label  className="text-sm">Confirm Password</label>
+					
+				</div>
+				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md bg-cyan-700 text-white border-none " />
+			</div>
+			
+		</div>
+		<div className="space-y-2">
+			<div>
+				<button type="submit" className="w-full px-8 py-3 font-semibold rounded-md bg-cyan-700 text-white font-serif">Sign Up</button>
+               
+            </div>
+			<p className="px-6 text-sm text-center text-cyan-700 font-serif">Already have accout?
+				{/* <Link to="/login">Login Here</Link> */}
+			</p>
+		</div>
+	</form>
+    {/* {
+               signupError && <p className=" font-semibold text-red-500">{signupError}</p>
+            }
+            {
+                succes && <p className="text-green-700  font-semibol">{succes}</p>
+            } */}
+</div>
+      
+    </div>
+  </div>
+</div>
         </div>
     );
 };
