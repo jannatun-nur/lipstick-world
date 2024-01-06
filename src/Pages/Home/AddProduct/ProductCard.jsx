ba3c8d8a51e42dd379e60aec4177603b8bace682
jannatun-nur <jannatunur03@gmail.com>
2024-01-06 09:52:56 +0600

    addProduct 90%

--- src/Pages/Home/AddProduct/ProductCard.jsx
+++ src/Pages/Home/AddProduct/ProductCard.jsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from "react";
-
+import { FaTrashAlt } from "react-icons/fa";
+import { BiEdit } from "react-icons/bi";
 const ProductCard = () => {
    const [product, setProduct] = useState([]);
+
+   const handleDelete = (_id) =>{
+    
+   }
 
    useEffect(() => {
        fetch('http://localhost:5000/get-allbuys')
@@ -13,23 +18,34 @@ const ProductCard = () => {
 
    return (
     <> 
-   
-     <table className="table table-zebra bg-pink-200">
-           <thead>
-               <tr>
+   <h1 className="text-2xl text-pink-600 text-center border border-pink-500 w-2/12 mx-auto mb-4 font-serif">My Orders</h1>
+     <table className="table w-9/12 mx-auto">
+           <thead >
+               <tr className="bg-pink-400 text-white font-serif text-xl">
                   <th>Name</th>
                   <th>Email</th>
                   <th>Category</th>
                   <th>Price</th>
+                  <th>Action</th>
+                  
                </tr>
            </thead>
            <tbody>
                {product.map((products, index) => (
                   <tr key={index}>
-                      <td>{products.name}</td>
-                      <td>{products.email}</td>
-                      <td>{products.category}</td>
-                      <td>{products.price}</td>
+                      <td className="text-xl text-gray-800 font-semibold">{products.name}</td>
+                      <td className="text-xl text-gray-800 font-semibold">{products.email}</td>
+                      <td className="text-xl text-gray-800 font-semibold">{products.category}</td>
+                      <td className="text-xl text-gray-800 font-semibold">{products.price}</td>
+                      <td className="text-xl">
+                        <button className="text-red-500 pr-10"
+                        onClick={()=>{handleDelete._id}}
+                        ><FaTrashAlt /></button>
+                        <button className="text-green-500"><BiEdit /></button>
+                      
+                      </td>
+                     
+                  
                   </tr>
                ))}
            </tbody>
