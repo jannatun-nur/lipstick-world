d5ba8d831dc559f36b03e652d01166c377300145
jannatun-nur <jannatunur03@gmail.com>
2024-01-07 18:25:58 +0600

    popular 60%

--- src/Pages/Home/Popular/Popular.jsx
+++ src/Pages/Home/Popular/Popular.jsx
@@ -20,7 +20,7 @@ useEffect(()=>{
         <div>
             
 
-            <div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
                 {
                     popular.map( populars =><PopularCard key={populars.name} populars={populars}></PopularCard>)
                 }
--- src/Pages/Home/Popular/PopularCard.jsx
+++ src/Pages/Home/Popular/PopularCard.jsx
@@ -1,11 +1,45 @@
-
-
-const PopularCard = () => {
-    return (
-        <div>
+import { Link } from "react-router-dom";
+import { FaBox } from "react-icons/fa";
+const PopularCard = ({ populars }) => {
+  const { name ,image_url ,in_stock, price} = populars;
+  return (
+    <div>
+       <div className="rounded-md shadow-md sm:w-52 bg-white text-pink-700 border border-black">
+        
+        <img
+          src={image_url}
+          alt=""
+          className="object-cover object-center w-52  dark:bg-gray-500"
+        />
+        <div className="p-3">
+          <div className="">
+          
+          </div>
+          <div className="flex flex-wrap items-center pb-1">
             
+          </div>
+          <div className="text-black font-semibold">
+            <div>
+            <p className="text-sm">
+              <span className="text-2xl font-bold">{name}</span>
+            </p>
+           
+            </div>
+            
+           <p className="text-xl font-bold">
+            $      {price}
+           </p>
+           <p className=" flex gap-2 text-2xl font-bold">
+          <span className="mt-2"> <FaBox /></span> {in_stock}
+           </p>
+          </div>
+         <Link to="buylipstick">
+         <button className="px-2 border border-pink-500 rounded-lg mt-2 font-semibold"></button>
+         </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default PopularCard;
